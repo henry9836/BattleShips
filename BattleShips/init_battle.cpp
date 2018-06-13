9124875d48f448a11e044cc9003a511c68ca67af
Nitro <37818378+henry9836@users.noreply.github.com>
2018-06-13 18:43:38 +1200

    stuff was done why so slow now

--- BattleShips/init_battle.cpp
+++ BattleShips/init_battle.cpp
@@ -125,6 +125,55 @@ bool checkforships(int ship, int xcord, int ycord, bool horizontal, int debug) {
 	return result;
 }
 
+void draw_grids(int debug, string name) {
+	system("cls");
+
+	int p1_1 = 0;
+	int p2_1 = 0;
+
+	for (int h = 0; h < 3; h++) {
+		for (int i = 0; i < 10; i++)    //This loops on the rows.
+		{
+			for (int j = 0; j < 10; j++) //This loops on the columns
+			{
+				if (h == 1) {
+					if (p1_1 == 0) {
+						GotoXY(0, 0);
+						p1_1 = 1;
+						cout << name << "'s Ships:" << endl;
+					}
+					cout << p1_board[i][j];
+				}
+				if (debug == 1) {
+					if (h == 2) {
+						if (p2_1 == 0) {
+							//GotoXY(100, 0);
+							p2_1 = 1;
+							cout << "AI's Ships: " << endl;
+						}
+						cout << p2_board[i][j];
+					}
+				}
+			}
+			cout << "\t";
+			for (int o = 0; o < 10; o++) //This loops on the columns of second grid
+			{
+				if (h == 1) {
+					cout << p1_board2[i][o];
+				}
+				if (debug == 1) {
+					if (h == 2) {
+						cout << p2_board2[i][o];
+					}
+				}
+
+			}
+
+			cout << endl;
+		}
+		cout << endl << endl << endl;
+	}
+}
 
 bool placeships(int x, int shiptype, int xcord, int ycord, bool horizontal, int debug, string name, bool boatsplaced) {
 
@@ -247,57 +296,6 @@ bool placeships(int x, int shiptype, int xcord, int ycord, bool horizontal, int 
 		return false;
 	}
 }
-
-void draw_grids(int debug, string name) {
-	system("cls");
-
-	int p1_1 = 0;
-	int p2_1 = 0;
-
-	for (int h = 0; h < 3; h++) {
-		for (int i = 0; i < 10; i++)    //This loops on the rows.
-		{
-			for (int j = 0; j < 10; j++) //This loops on the columns
-			{
-				if (h == 1) {
-					if (p1_1 == 0) {
-						GotoXY(0, 0);
-						p1_1 = 1;
-						cout << name << "'s Ships:" << endl;
-					}
-					cout << p1_board[i][j];
-				}
-				if (debug == 1) {
-					if (h == 2) {
-						if (p2_1 == 0) {
-							//GotoXY(100, 0);
-							p2_1 = 1;
-							cout << "AI's Ships: " << endl;
-						}
-						cout << p2_board[i][j];
-					}
-				}
-			}
-			cout << "\t";
-			for (int o = 0; o < 10; o++) //This loops on the columns of second grid
-			{
-				if (h == 1) {
-					cout << p1_board2[i][o];
-				}
-				if (debug == 1) {
-					if (h == 2) {
-						cout << p2_board2[i][o];
-					}
-				}
-
-			}
-
-			cout << endl;
-		}
-		cout << endl << endl << endl;
-	}
-}
-
 
 int start_init(int debug, string name) {
 
